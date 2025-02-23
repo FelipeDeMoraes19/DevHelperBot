7262e75d99d439c151fec68e40a19bf41338f5f3
FelipeDeMoraes19 <moorraes16@gmail.com>
2025-02-23 10:07:30 -0300

    add dependency to get current user from token

---
+++ app/auth/dependencies.py
@@ -0,0 +1,30 @@
+from fastapi import Depends, HTTPException, status
+from fastapi.security import OAuth2PasswordBearer
+from jose import JWTError, jwt
+from sqlalchemy.ext.asyncio import AsyncSession
+from app.auth.auth_utils import SECRET_KEY, ALGORITHM
+from app.config.database import get_db
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/auth/login")
+
+async def get_current_user(token: str = Depends(oauth2_scheme), db: AsyncSession = Depends(get_db)):
+    credentials_exception = HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="Token de credenciais inválido ou ausente",
+        headers={"WWW-Authenticate": "Bearer"},
+    )
+
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        user_id: str = payload.get("sub")
+        if user_id is None:
+            raise credentials_exception
+    except JWTError:
+        raise credentials_exception
+
+    user = await db.execute("SELECT * FROM users WHERE id = :id LIMIT 1", {"id": user_id})
+    user_row = user.fetchone()
+    if not user_row:
+        raise credentials_exception
+
+    return user_row
